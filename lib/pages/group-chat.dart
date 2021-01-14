575fef0c65208b9ee2adee35d381ed550ff2665c
matilde-ribeiro <matilde.ribeiro@sky.uk>
2021-01-14 09:56:59 +0000

    added a file with the name of each one of the routes defined in the main to be easier to separate work from here

---
+++ lib/pages/group-chat.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GroupChat extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text('GroupChat'),
+      ),
+    );
+  }
+}
